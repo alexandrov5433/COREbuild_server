3244a316a7681eac24b4dce5fa21a6112c24daf1
alexandrov5433 <alexandrov5433@gmail.com>
2025-03-03 22:58:19 +0100

    Product creation query added.

---
+++ src/data/product.ts
@@ -0,0 +1,29 @@
+import { ProductCreationData } from "./definitions.js";
+import { pool } from "./postgres.js";
+
+export async function createProduct(productData: ProductCreationData) {
+    const client = await pool.connect();
+    try {
+        const specsDocIDVal = productData.specsDocID || 'DEFAULT';
+        const picturesVal = productData.pictures ? `'{${productData.pictures.join(', ')}}'` : 'DEFAULT';
+        return await client.query(`
+            INSERT INTO "product" VALUES(
+                DEFAULT,
+                '${productData.name}',
+                '${productData.description}',
+                ${productData.price},
+                ${productData.stockCount},
+                '${productData.manufacturer}',
+                ${specsDocIDVal},
+                ${productData.thumbnailID},
+                ${picturesVal},
+                DEFAULT)
+            RETURNING *
+            `)
+    } catch (e) {
+        console.error(e.message);
+        return null;
+    } finally {
+        client.release();
+    }
+}
