e18ccc94ff5bfd1256006f1d3ad8b6d5ac1b02b3
alexandrov5433 <alexandrov5433@gmail.com>
2025-03-03 22:58:44 +0100

    Endpoint for product creation added.

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,18 @@ import login from "../handlers/user/login.js";
 import register from "../handlers/user/register.js";
 import logout from "../handlers/user/logout.js";
 import validateCookie from "../handlers/user/validateCookie.js";
+import addProduct from "../handlers/product/add-product.js";
 
 const router = Router();
+
+// user
 router.post('/api/login', login);
 router.post('/api/register', register);
 router.get('/api/logout', logout);
 router.get('/api/validate-cookie', validateCookie);
+
+// product
+router.post('/api/add-product', addProduct);
 
 router.all('*', (req: Request, res: Response) => {
     res.redirect('/index.html');
