40e204310a3116ef8a5fa20323efd7831520760a
alexandrov5433 <alexandrov5433@gmail.com>
2025-03-03 22:56:50 +0100

    File upload lib configuration.

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,13 +1,15 @@
 import { json, static as static_ } from "express";
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
+// import cors from 'cors';
 import { checkCookie } from "../util/cookie.js";
 import { router } from "../router/index.js";
 import path from 'node:path';
 import { verifyDBConnection } from '../data/postgres.js'; //init Pool instance and check connection to DB
+import fileUpload from "express-fileupload";
 
 // const corsOrigin = process.env.CORS_ORIGIN || '';
 const appAssetsPath = path.resolve('./dist_app');
+const PDF_SIZE_LIMIT_MB = Number(process.env.PDF_SIZE_LIMIT_MB) || 4;
 
 export default async function configExpress(app: any) {
     try {
@@ -20,6 +22,15 @@ export default async function configExpress(app: any) {
         app.use(cookieParser());
         app.use(checkCookie);
         app.use(static_(appAssetsPath)); //must be before router
+        app.use(fileUpload({ // uploads files into RAM; files are then moved to storage in handler function
+            limits: {
+                fileSize: PDF_SIZE_LIMIT_MB * 1024 * 1024
+            },
+            safeFileNames: true, // strip non-alphanumeric characters except dashes
+            preserveExtension: 4,  // allow extentions of length up to 4 char - eg. jpeg
+            abortOnLimit: true, // Returns a HTTP 413 when the file is bigger than the size limit
+            responseOnLimit: `One or more files exceed the ${PDF_SIZE_LIMIT_MB}MB size limit.`
+        }));
         app.use(router);
         await verifyDBConnection(); //implement fallback handlers?
     } catch (e) {
