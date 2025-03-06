861979f14eca70aea19b22bd12dda6c4dddee9e8
alexandrov5433 <alexandrov5433@gmail.com>
2025-03-06 19:49:51 +0100

    Getting file name from DB by the file id.

--- src/data/file.ts
+++ src/data/file.ts
@@ -16,3 +16,17 @@ export async function createFile(fileName: string) {
         client.release();
     }
 }
+
+export async function getFileNameById(fileID: number): Promise<string | null> {
+    const client = await pool.connect();
+    try {
+        const res = await client.query(`
+            SELECT * FROM file WHERE "fileID"=$1`, [fileID]);
+        return res?.rows[0]?.name || null;
+    } catch (e) {
+        console.error(e.message);
+        return null;
+    } finally {
+        client.release();
+    }
+}
