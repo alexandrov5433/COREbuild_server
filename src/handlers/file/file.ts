9228e8825e9366c6ef9de3a620854b3528dcdb44
alexandrov5433 <alexandrov5433@gmail.com>
2025-03-06 19:50:31 +0100

    File streaming functionality added.

---
+++ src/handlers/file/file.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from "express";
+import path from 'node:path';
+import fsp from 'node:fs/promises';
+import sp from 'node:stream/promises';
+import { getFileNameById } from "../../data/file.js";
+
+const PICS_STORAGE_PATH = path.resolve('./fileStorage/pics');
+const DOCS_STORAGE_PATH = path.resolve('./fileStorage/docs');
+
+export default async function file(req: Request, res: Response) {
+    try {
+        const fileid = req.params.fileid || null;
+        const picOrDoc = req.params.picOrDoc || null;
+
+        if (!fileid || !Number(fileid)) {
+            res.status(400);
+            res.json({
+                msg: 'File id is required in order to return a file.'
+            });
+            return;
+        }
+        if (!picOrDoc || !['pic', 'doc'].includes(picOrDoc)) {
+            res.status(400);
+            res.json({
+                msg: 'Missing parameter picOrDoc.'
+            });
+            return;
+        }
+        const filename = await getFileNameById(Number(fileid));
+        if (!filename) {
+            res.status(400);
+            res.json({
+                msg: `A file with fileID: "${fileid}" was not found.`
+            });
+            return;
+        }
+        const filePath = path.join({
+            'pic': PICS_STORAGE_PATH,
+            'doc': DOCS_STORAGE_PATH,
+        }[picOrDoc], filename);
+
+        const stats = await fsp.stat(filePath);
+        if (!stats) {
+            res.status(400);
+            res.json({
+                msg: `No file with file name: "${filename}" was found in storage.`
+            });
+            return;
+        }
+        const fileHandle = await fsp.open(filePath, 'r');
+        const fileSteam = fileHandle.createReadStream();
+        const contentType = picOrDoc === 'pic' ?
+            (filename.endsWith('.png') ?
+                'image/png'
+                : 'image/jpeg'
+            )
+            : 'application/pdf';
+        res.status(200);
+        res.set({
+            'Content-Type': contentType,
+            'Content-Length': stats.size,
+        });
+        await sp.pipeline(fileSteam, res);
+        res.end();
+    } catch (e) {
+        res.status(500);
+        res.json({
+            msg: `Error: ${(e as Error).message}`
+        });
+        res.end();
+    }
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,9 @@ import register from "../handlers/user/register.js";
 import logout from "../handlers/user/logout.js";
 import validateCookie from "../handlers/user/validateCookie.js";
 import addProduct from "../handlers/product/add-product.js";
+import productDetails from "../handlers/product/product-details.js";
+import productsCatalog from "../handlers/product/products-catalog.js";
+import file from "../handlers/file/file.js";
 
 const router = Router();
 
@@ -16,6 +19,11 @@ router.get('/api/validate-cookie', validateCookie);
 
 // product
 router.post('/api/add-product', addProduct);
+router.get('/api/product-details', productDetails);
+router.get('/api/products-catalog', productsCatalog);
+
+// file
+router.get('/api/file/:picOrDoc/:fileid', file);
 
 router.all('*', (req: Request, res: Response) => {
     res.redirect('/index.html');
