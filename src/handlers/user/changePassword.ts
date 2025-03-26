10d31fa5cf775c79b998f558e399ad4ae61c58b2
alexandrov5433 <alexandrov5433@gmail.com>
2025-03-26 11:22:52 +0100

    Unused code deleted.

--- src/handlers/user/changePassword.ts
+++ src/handlers/user/changePassword.ts
@@ -55,20 +55,3 @@ export default async function changePassword(req: Request, res: Response) {
         res.end();
     }
 }
-
-function validateNewProfileDetails(newProfileDetails: NewProfileDetails) {
-    const emailValid = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test(newProfileDetails.email || '');
-    const firstnameValid = /^[A-Za-z]{1,50}$/.test(newProfileDetails.firstname || '');
-    const lastnameValid = /^[A-Za-z]{1,50}$/.test(newProfileDetails.lastname || '');
-    const addressValid = /^[A-Za-z0-9\., -]+$/.test(newProfileDetails.address || '');
-
-    return {
-        currentPassword: emailValid,
-        newPassword: firstnameValid,
-    };
-}
-
-function isInformationValid(validationObject: any) {
-    const result = Object.values(validationObject).find(e => e === false);
-    return result ? false : true;
-}
