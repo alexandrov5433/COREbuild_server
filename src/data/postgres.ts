ff9f303bbbcf57e278a170b26f336178db1635d9
alexandrov5433 <alexandrov5433@gmail.com>
2025-03-30 21:46:38 +0200

    idleTimeoutMillis changed to allow clients to timeout after a long time - 10 sec. Value 0 only worsened the problem it was supposed to fix....

--- src/data/postgres.ts
+++ src/data/postgres.ts
@@ -4,7 +4,7 @@ const { Pool } = pg;
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production' ? true : false,
-  idleTimeoutMillis: 0,  // number of milliseconds a client must sit idle in the pool and not be checked out
+  idleTimeoutMillis: 10000,  // number of milliseconds a client must sit idle in the pool and not be checked out
   // before it is disconnected from the backend and discarded
   // default is 10000 (10 seconds) - set to 0 to disable auto-disconnection of idle client
   // ---->>> check pg_error_1.txt
