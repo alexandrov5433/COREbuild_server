443e7585882547d50489dd3d43741409e7d649b2
alexandrov5433 <alexandrov5433@gmail.com>
2025-04-02 22:51:57 +0200

    Update.

--- dist_server/handlers/review/addNewReview.js
+++ dist_server/handlers/review/addNewReview.js
@@ -21,6 +21,14 @@ export default async function addNewReview(req, res) {
             isVerifiedPurchase: false,
             productID: Number(req.body.productID) || null
         };
+        if (!reviewData.rating || !reviewData.comment) {
+            res.status(400);
+            res.json({
+                msg: 'Both rating and comment are needed to submit a review.'
+            });
+            res.end();
+            return;
+        }
         if (!reviewData.productID) {
             res.status(400);
             res.json({
